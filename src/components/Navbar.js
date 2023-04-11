b9edda03a30f16b8c3b3d986a7f97ca2f89fe4a6
LalitBhadu <lalitbhadu1111@gmail.com>
2023-04-11 13:11:10 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Home } from "@mui/icons-material";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Home color="success" />
+
+      <h2>this is h2</h2>
+    </div>
+  );
+}
+
+export default Navbar;
